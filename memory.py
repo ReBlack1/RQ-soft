03fd5b9fabaaeb53def20a6222c2fd3d6ec52470
Евгений <krisa12300@yandex.ru>
2018-05-04 17:33:31 +0300

    Пополнил базовые смещения функции для фарма

--- memory.py
+++ memory.py
@@ -88,16 +88,31 @@ def _getAddress_from_bytes(_bytes, pid = None, buffer_size = None):
 
 def get_move_dict():
     MOVE_DICTIONARY = {
-    "hp" : [0x7CBFD4, 0x4, 0x204],
-    "fight" : [0x684888, 0x8, 0x8, 0x14, 0x8, 0xD8],
-    "target_on_screen" : [0x9DD128],
-    'mob_num' : [0x7CBFD4, 0x18, 0xB4494, 0x2D4],
-    'cum_value' : [0x7CBFE4, 0x54, 0x44],
-    'x_pos' : [0x9E0714],
-    'y_pos' : [0x9E0718],
-    'z_pos' : [0x9E071C]
+    "hp"                 :               [0x7CBFD4, 0x4, 0x204],
+    "fight"              :               [0x684888, 0x8, 0x8, 0x14, 0x8, 0xD8],
+    "target_on_screen"   :               [0x9DD128],
+    'cum_value'          :               [0x7CBFE4, 0x54, 0x44],
+    'my_person_base'     :               [0x7CBFD4, 0x18, 0xB4494, 0x0],
+    'x_pos'              :               [0x9E0714],
+    'y_pos'              :               [0x9E0718],
+    'z_pos'              :               [0x9E071C],
+    'x_ang'              :               [0x9DCEAC],
+    'y_ang'              :               [0x9DD064],
     }
     return MOVE_DICTIONARY
+
+def get_move_obj():
+    return {
+    'id'                 :               0x14,
+    'magic_cast'         :               0x18,
+    'all_cast'           :               0x58,
+    'x_pos'              :               0xF4,
+    'y_pos'              :               0xF8,
+    'z_pos'              :               0xFC,
+    'hp'                 :               0x218,
+    'anim'               :               0x21C,
+    'target'             :               0x2D4,
+    }
 
 def _getAddress_dict(MOVE_DICT = None,pid = None):
     if pid== None:
@@ -114,9 +129,23 @@ def _write_to_address(address, input_data,data_size=4 ,pid=None):
         pid = _get_pid('rqmain.exe')
     h_process = windll.kernel32.OpenProcess( PROCESS_ALL_ACCESS, False, int(pid) )
     written = c_int(1)
-    BufferLocal = ctypes.create_string_buffer(data_size)
     if type(input_data) == int:
+        BufferLocal = ctypes.create_string_buffer(data_size)
         BufferLocal.value = input_data.to_bytes(data_size, byteorder='little')
     elif type(input_data) == bytes:
+        data_size = len(input_data)
+        BufferLocal = ctypes.create_string_buffer(data_size)
         BufferLocal.value = input_data
     ans = windll.kernel32.WriteProcessMemory(h_process, address, BufferLocal, data_size, byref(written))
+
+def _make_my_target_func(pid=None):
+    if pid == None:
+        pid = _get_pid('rqmain.exe')
+    target_func = b'\x8B\x80\xD4\x02\x00\x00\x85\xC0\x74\x04\x83\xC0\xEC\xC3\x33\xC0'
+    my_target_func = b'\x8B\x80\xD4\x02\x00\x00\x85\xC0\x74\x0B\x83\xC0\xEC\x90\x90\x90\x90\x90\x90\x90\xC3\x90\x90\xC3'
+    addr_target_func = _getAddress_from_bytes(target_func, pid=pid)
+    if addr_target_func:
+        _write_to_address(addr_target_func, my_target_func, pid=pid) #Если адрес нашелся - меняем переписываем функцию
+    else:
+        addr_target_func = _getAddress_from_bytes(my_target_func, pid=pid) #Или уже заменял, тогда вернет новый адрес, или ошибка-вернет None
+    return addr_target_func
--- soft_manager.py
+++ soft_manager.py
@@ -3,12 +3,6 @@
 import memory
 import math
 
-pid = memory._get_pid()
-ADDR = memory._getAddress_dict(pid=pid)
-print(hex(ADDR['mob_num']))
-target_func = b'\x8B\x80\xD4\x02\x00\x00\x85\xC0\x74\x04\x83\xC0\xEC\xC3\x33\xC0'
-print(hex(memory._getAddress_from_bytes(target_func, pid=pid)))
-my_target_func = b'\x8B\x80\xD4\x02\x00\x00\x85\xC0\x74\x0B\x83\xC0\xEC\x90\x90\x90\x90\x90\x90\x90\xC3\x90\x90\xC3'
 #Апроксимация: погрешность +- 3 м.
 def _convert_cord(cord):
     del1 = 1049986912
@@ -22,3 +16,41 @@ def _convert_cord(cord):
         cord /= 1000
         return -1 * int(0.00000000000127141154 * math.pow(cord, 3) - 0.00000009052023104661 * math.pow(cord, 2) + 0.00156103066680002289 * cord - 1.62538467533931907383)
 
+def _pred_proc(pid=None, ADDR=None):
+    if pid == None:
+        pid = memory._get_pid('rqmain.exe')
+    if ADDR == None:
+        ADDR = memory._getAddress_dict(pid=pid)
+    addr_target_func = memory._make_my_target_func(pid) #подготовка функции таргетирования
+    if addr_target_func != None:
+        ADDR['target_func'] = addr_target_func
+    return ADDR
+
+def _kill_id(ADDR, BASE, id, pid=None):
+    if pid == None:
+        pid = memory._get_pid('rqmain.exe')
+    target_func_addr = ADDR['target_func'] + 0xA
+    my_target_addr = ADDR['my_person_base'] + BASE['target']
+    mov = b'\xB8'
+    if type(id) == int:
+        id = id.to_bytes(4, byteorder='little')
+    new_tagret = mov + id
+    memory._write_to_address(my_target_addr, b'\x00\x00\x00\x00', pid=pid)
+    #Могут быть проблемы с агрящимся на меня мобом
+    memory._write_to_address(target_func_addr, new_tagret, 5, pid=pid)
+    return True
+
+def _save_id(ADDR, BASE, id, pid=None):
+    if pid == None:
+        pid = memory._get_pid('rqmain.exe')
+    old_target = b'\x83\xC0\xEC\x90\x90'
+    target_func_addr = ADDR['target_func'] + 0xA
+    my_target_addr = ADDR['my_person_base'] + BASE['target']
+
+
+
+pid = memory._get_pid()
+ADDR = _pred_proc(pid)
+BASE = memory.get_move_obj()
+exit()
+_kill_id(ADDR, BASE, 0x3B0F074, pid)
